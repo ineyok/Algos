5324c4cb360589549cdd431e43e63e7347541966
ineyok <redkindany@mail.ru>
2024-12-30 22:00:13 +0400

    Update Ordered set.cpp

--- Ordered set.cpp
+++ Ordered set.cpp
@@ -2,13 +2,13 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
-# множество <int>
+// множество <int>
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
-# словарь <int, int>
+// словарь <int, int>
 typedef tree<int, int, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
-# мультимножество <int, index>
+// мультимножество <int, index>
 typedef tree<pair<int, int>, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 
