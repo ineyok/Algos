60b5fde59e661d4f76d8c9c6a37f1c19f88503b8
ineyok <redkindany@mail.ru>
2025-01-08 16:24:51 +0400

    Update Make personal #ifdef .cpp

--- Make personal #ifdef .cpp
+++ Make personal #ifdef .cpp
@@ -6,3 +6,9 @@
 if (YOUR_DEFINE_VARIABLE)
     add_definitions(-DYOUR_DEFINE_VARIABLE=1)
 endif (YOUR_DEFINE_VARIABLE)
+
+// Теперь в main.cpp можно писать
+#ifdef YOUR_DEFINE_VARIABLE
+    ...
+#endif
+// И туда не будет заходить в тестирующей системе. Можно объявить свой файловый ввод-вывод
