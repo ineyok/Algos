441923dd9c83ef823b638be664f471863fa5bb07
ineyok <redkindany@mail.ru>
2025-01-10 23:07:37 +0400

    Create Ford-Bellman + find neg. cycle .cpp

---
+++ Ford-Bellman + find neg. cycle .cpp
@@ -0,0 +1,55 @@
+// O(V * E)
+
+
+// Находит кратчайшие расстояния от одной вершины до всех остальных в !графе с отрицательными циклами!
+ll dp[mxN];
+int pr[mxN], n;
+vector<vector<int>> edges;
+void find_negative_cycle(int start) {
+    fill(dp, dp + mxN, LINF);
+    dp[start] = 0;
+    for(int i = 0; i < n; ++i) {
+        for (auto &el: edges) {
+            int v = el[0], to = el[1], w = el[2];
+            if (dp[to] > dp[v] + w) {
+                dp[to] = dp[v] + w;
+                pr[to] = v;
+            }
+        }
+    }
+}
+
+
+// Находит случайный отрицательный цикл в графе
+ll dp[mxN];
+int pr[mxN], n;
+vector<vector<int>> edges;
+vector<int> find_negative_cycle() {
+    fill(dp, dp + mxN, 0);
+    for(int i = 0; i < n; ++i) {
+        for (auto &el: edges) {
+            int v = el[0], to = el[1], w = el[2];
+            if (dp[to] > dp[v] + w) {
+                dp[to] = dp[v] + w;
+                pr[to] = v;
+            }
+        }
+    }
+    for(auto & el : edges) {
+        int u = el[0], v = el[1], w = el[2];
+        if(dp[v] > dp[u] + w) {
+            for(int i = 0; i < n; ++i) v = pr[v];
+            int vv = v;
+            vector<int> ans;
+            while(pr[v] != vv) {
+                ans.push_back(v);
+                v = pr[v];
+            }
+            ans.push_back(v);
+            ans.push_back(ans[0]);
+            reverse(all(ans));
+            return ans;
+        }
+    }
+    return {};
+}
