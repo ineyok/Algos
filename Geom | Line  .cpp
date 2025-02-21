a679b94ff65106ea75ac00e0f9c9c8355ac752c3
ineyok <redkindany@mail.ru>
2025-02-21 16:22:31 +0400

    Create Geom | Line  .cpp

---
+++ Geom | Line  .cpp
@@ -0,0 +1,24 @@
+struct Line {
+    double A, B, C;
+    Point p1, p2, n, r;
+
+    void recalc_vecs() {
+        n = {A, B};
+        r = {-B, A};
+    }
+
+    Line() {}
+    Line(Point a, Point b) : A(a.y - b.y), B(b.x - a.x), C(a % b), p1(a), p2(b) {
+        recalc_vecs();
+    }
+    Line(const Line &ref) = default;
+
+    Line norm() const {
+        Line ans(*this);
+        double k = sqrt(A * A + B * B);
+        if (C > 0) k *= -1;
+        ans.A /= k, ans.B /= k, ans.C /= k;
+        if (ans.A < 0 || (ans.A == 0 && ans.B < 0)) ans.A *= -1, ans.B *= -1, ans.C *= -1;
+        return ans;
+    }
+};
