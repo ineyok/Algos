c77b8f0125933257f02c78cb04aea386426dd499
ineyok <redkindany@mail.ru>
2025-02-26 15:11:04 +0400

    Update Geom | Line  .cpp

--- Geom | Line  .cpp
+++ Geom | Line  .cpp
@@ -1,25 +1,31 @@
-struct Line {
-    double A, B, C;
+template<typename T>
+struct LineT {
+    T A, B, C;
     Point p1, p2, n, r;
 
-    void recalc_vecs() {
+    void help() {
+        if (C > 0) A *= -1, B *= -1, C *= -1;
+        if (A < 0 || (A == 0 && B < 0)) A *= -1, B *= -1, C *= -1;
         n = {A, B};
         r = {-B, A};
     }
 
-    Line() {}
-    Line(Point a, Point b) : A(a.y - b.y), B(b.x - a.x), C(a % b), p1(a), p2(b) {
-        recalc_vecs();
+    LineT() {}
+    LineT(Point a, Point b) : A(a.y - b.y), B(b.x - a.x), C(a % b), p1(a), p2(b) {
+        help();
     }
-    Line(const Line &ref) = default;
+    LineT(T A, T B, T C) : A(A), B(B), C(C) {
+        help();
+    }
+    LineT(const LineT &ref) = default;
 
-    Line norm() const {
-        Line ans(*this);
+    LineT norm() const {
+        LineT ans(*this);
         double k = sqrt(A * A + B * B);
-        if (C > 0) k *= -1;
         ans.A /= k, ans.B /= k, ans.C /= k;
-        if (ans.A < 0 || (ans.A == 0 && ans.B < 0)) ans.A *= -1, ans.B *= -1, ans.C *= -1;
-        ans.recalc_vecs();
+        ans.help();
         return ans;
     }
 };
+
+using Line = LineT<ll>;
