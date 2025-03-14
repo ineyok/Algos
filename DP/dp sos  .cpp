d9233495498b6ad1de93f984d3862ee5c2c92221
ineyok <redkindany@mail.ru>
2025-03-14 17:59:31 +0400

    Update dp sos  .cpp

--- DP/dp sos  .cpp
+++ DP/dp sos  .cpp
@@ -1,4 +1,5 @@
 // sum of subsets
+// sos[bit][mask] - сумма по подмаскам mask, которые могут отличаться от mask в первых bit битах
 // O(n * log n)
 for(int bit = 0; bit < K; ++bit) {
     for(int mask = 0; mask < (1 << K); ++mask) {
