df6358253a9189ac18552eb80d65c133ce41aa4d
ineyok <redkindany@mail.ru>
2025-03-15 09:58:23 +0400

    Create CHT  .cpp

---
+++ DP/CHT  .cpp
@@ -0,0 +1,34 @@
+struct CHT {
+    struct Line {
+        ll k, c, start;
+        int i;
+        Line(ll k, ll c, ll start, int i) : k(k), c(c), start(start), i(i) {}
+    };
+    vector<Line> vec;
+
+    CHT()=default;
+
+    int query(ll x) {
+        int lb = 0, rb = siz(vec);
+        while(lb + 1 < rb) {
+            int mid = (lb + rb) / 2;
+            if(vec[mid].start <= x) lb = mid;
+            else rb = mid;
+        }
+        return vec[lb].i;
+    }
+
+    void add(ll k, ll c, int i) {
+        if(vec.empty()) {
+            vec = {{k, c, -INF, i}};
+        }
+        else {
+            ll xp = (c - vec.back().c) / (vec.back().k - k);
+            while (xp <= vec.back().start) {
+                vec.pop_back();
+                xp = (c - vec.back().c) / (vec.back().k - k);
+            }
+            vec.push_back({k, c, xp, i});
+        }
+    }
+};
