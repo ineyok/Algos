c96f01278ff6021853435a49364e29cb7f01d466
ineyok <redkindany@mail.ru>
2025-03-16 11:45:11 +0400

    Update Djikstra  .cpp

--- Graphs/Shortest Paths/Djikstra  .cpp
+++ Graphs/Shortest Paths/Djikstra  .cpp
@@ -7,12 +7,12 @@ dist[S] = 0;
 set<pair<ll, int>> st;
 st.insert({0, S});
 while(siz(st)) {
-  int v = st.begin()->second; st.erase(st.begin());
-  for(auto & [to, w] : g[v]) {
-    if(dist[to] > dist[v] + w) {
-      st.erase({dist[to], to});
-      dist[to] = dist[v] + w;
-      st.insert({dist[to], to});
+    int v = st.begin()->second; st.erase(st.begin());
+    for(auto & [to, w] : g[v]) {
+        if(dist[to] > dist[v] + w) {
+            st.erase({dist[to], to});
+            dist[to] = dist[v] + w;
+            st.insert({dist[to], to});
+        }
     }
-  }
 }
