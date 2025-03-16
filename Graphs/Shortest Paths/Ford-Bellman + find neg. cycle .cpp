d7cb47248c2d2f91360bbb0610da1a07d3d9aeb6
ineyok <redkindany@mail.ru>
2025-03-16 11:39:03 +0400

    Update Ford-Bellman + find neg. cycle .cpp

--- Graphs/Shortest Paths/Ford-Bellman + find neg. cycle .cpp
+++ Graphs/Shortest Paths/Ford-Bellman + find neg. cycle .cpp
@@ -26,23 +26,34 @@ void Ford_Bellman(int start) {
     }
 }
 
-// Находит случайный отрицательный цикл в СВЯЗНОМ графе
-vector<int> find_negative_cycle() {
-    Ford_Bellman(0);
-    for(int u = 0; u < n; ++u) {
-        if(dist[u] == -LINF) {
-            int v = u;
-            for(int i = 0; i < n; ++i) v = pr[v];
+// Находит случайный отрицательный цикл в графе
+// (Запускаем Ford_B от всех вершин)
+ll dist[mxN];
+int pr[mxN], n;
+vector<vector<int>> edges;
+vector<int> find_neg_cycle() {
+    fill(dist, dist + mxN, 0);
+    for(int i = 0; i < n - 1; ++i) {
+        for (auto &el : edges) {
+            int v = el[0], to = el[1], w = el[2];
+            if (dist[to] > dist[v] + w) {
+                dist[to] = dist[v] + w;
+                pr[to] = v;
+            }
+        }
+    }
+    for(auto & el : edges) {
+        int v = el[0], to = el[1], w = el[2];
+        if(dist[to] > dist[v] + w) {
+            for(int i = 0; i < n - 1; ++i) v = pr[v];
             int vv = v;
-            vector<int> ans;
-            while(pr[v] != vv) {
-                ans.push_back(v);
+            vector<int> cycle;
+            while(cycle.empty() || v != vv) {
+                cycle.push_back(v);
                 v = pr[v];
             }
-            ans.push_back(v);
-            ans.push_back(ans[0]);
-            reverse(all(ans));
-            return ans;
+            reverse(all(cycle));
+            return cycle;
         }
     }
     return {};
