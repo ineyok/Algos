10112bd7a770c6a0bfbae67204f4b2a2b470dcd6
ineyok <redkindany@mail.ru>
2025-03-16 11:44:19 +0400

    Create SPFA  .cpp

---
+++ Graphs/Shortest Paths/SPFA  .cpp
@@ -0,0 +1,24 @@
+// O(V * E)
+
+// Замена Ford_Bellman с меньшей константой
+
+ll dist[mxN];
+int inq[mxN];
+void spfa(int S) {
+    fill(dist, dist + mxN, LINF);
+    fill(inq, inq + mxN, 0);
+    dist[S] = 0;
+    queue<int> q;
+    q.push(S);
+    while(siz(q)) {
+        int v = q.front(); q.pop();
+        inq[v] = 0;
+        for(auto & [to, f, cap, cost, obr] : g[v]) {
+            if(f < cap && ckmin(dist[to], dist[v] + cost)) {
+                inde[to] = obr;
+                if(!inq[to]) q.push(to);
+                inq[to] = 1;
+            }
+        }
+    }
+}
