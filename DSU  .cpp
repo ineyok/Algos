1876e48bf0672da8d6843bc20c2c686c71085ea2
ineyok <redkindany@mail.ru>
2025-03-19 14:01:56 +0400

    Create DSU  .cpp

---
+++ DSU  .cpp
@@ -0,0 +1,20 @@
+// Обе эвристики, O(1) на запрос
+
+struct DSU {
+    int n;
+    vector<int> pr, sz;
+    
+    DSU(int n) : n(n), pr(n), sz(n, 1) {
+        iota(all(pr), 0);
+    }
+    
+    int root(int v) { return pr[v] = (pr[v] == v ? v : root(pr[v])); }
+    
+    void merge(int a, int b) {
+        a = root(a), b = root(b);
+        if(a == b) return;
+        if(sz[a] < sz[b]) swap(a, b);
+        sz[a] += sz[b];
+        pr[b] = a;
+    }
+};
