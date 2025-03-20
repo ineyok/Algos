291fa1ab95fd1de1b2abed9bc366f5b0e71d706f
ineyok <redkindany@mail.ru>
2025-03-20 23:27:05 +0400

    Create pattern  .cpp

---
+++ pattern  .cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+mt19937 rnd();
+#define all(x) x.begin(), x.end()
+#define siz(x) int(x.size())
+#define ceil(a, b) (((a) + (b) - 1) / b)
+const int INF = 2e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cout << fixed; cout.precision(8);
+#ifdef INEYOK
+    freopen("input.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    solve();
+}
