34ecd9dc2381ecfa25f48bf177b6824b0c173118
ineyok <redkindany@mail.ru>
2025-03-21 12:39:17 +0400

    Update !!!kuhn  .cpp

--- Graphs/!!!kuhn  .cpp
+++ Graphs/!!!kuhn  .cpp
@@ -1,1 +1,29 @@
 
+int used[mxN * 2], mt[mxN * 2], vers = 0;
+
+bool dfs(int v) {
+    if(used[v] == vers) return 0;
+    used[v] = vers;
+    for(int to : g[v]) {
+        if(mt[to] == -1) {
+            mt[to] = v;
+            return 1;
+        }
+    }
+    for(int to : g[v]) {
+        if(dfs(mt[to])) {
+            mt[to] = v;
+            return 1;
+        }
+    }
+    return 0;
+}
+int kuhn() {
+    int ans = 0;
+    fill(mt, mt + mxN * 2, -1);
+    for(int v = 0; v < m; ++v) {
+        vers++;
+        if(dfs(v)) ans++;
+    }
+    return ans;
+}
