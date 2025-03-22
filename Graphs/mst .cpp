e0bf62c746fcfad04b197e0089b26e6ccd71a81d
ineyok <redkindany@mail.ru>
2025-03-22 14:43:04 +0400

    Update and rename !!!mst to mst .cpp

---
+++ Graphs/mst .cpp
@@ -0,0 +1,27 @@
+// O(m)
+
+// Минимальное остовное дерево
+
+// Алгоритм Крускала
+struct DSU {
+    ...
+};
+
+int n;
+vector<vector<int>> edges;
+
+ll kruskal() {
+    DSU dsu(n);
+    sort(all(edges), [&](vector<int> & a, vector<int> & b) {
+        return a[2] < b[2];
+    });
+    ll sum = 0;
+    for(auto & el : edges) {
+        int a = el[0], b = el[1], w = el[2];
+        if(!dsu.connect(a, b)) {
+            sum += w;
+            dsu.merge(a, b);
+        }
+    }
+    return sum;
+}
