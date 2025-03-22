846c7ec5796ceaf8e6b01e283f2bfdf93de479b7
ineyok <redkindany@mail.ru>
2025-03-22 22:03:12 +0400

    Update Ordered set .cpp

--- Structures/Ordered set .cpp
+++ Structures/Ordered set .cpp
@@ -1,15 +1,14 @@
 #include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
 // множество <int>
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+using ordered_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // словарь <int, int>
-typedef tree<int, int, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+using ordered_map = tree<int, int, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // мультимножество <int, index>
-typedef tree<pair<int, int>, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+using ordered_set = tree<pair<int, int>, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
 
 
 ordered_set ost;
