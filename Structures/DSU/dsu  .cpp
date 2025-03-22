b27c0d7814e7f7dfc4c66353955a65fa2d018dcd
ineyok <redkindany@mail.ru>
2025-03-22 14:43:57 +0400

    Update dsu  .cpp

--- Structures/DSU/dsu  .cpp
+++ Structures/DSU/dsu  .cpp
@@ -1,15 +1,16 @@
 // O(1) на запрос
 
 struct DSU {
-    int n;
     vector<int> pr, sz;
-    
-    DSU(int n) : n(n), pr(n), sz(n, 1) {
+
+    DSU()=default;
+    DSU(int n) : pr(n), sz(n, 1) {
         iota(all(pr), 0);
     }
-    
+
     int root(int v) { return pr[v] = (pr[v] == v ? v : root(pr[v])); }
-    
+    bool connect(int a, int b) { return root(a) == root(b); }
+
     void merge(int a, int b) {
         a = root(a), b = root(b);
         if(a == b) return;
