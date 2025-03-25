6d3647f9a2a34acff019ecef4c8e5d08e99e17ea
ineyok <redkindany@mail.ru>
2025-03-25 14:00:08 +0400

    Update personal #ifdef .cpp

--- personal #ifdef .cpp
+++ personal #ifdef .cpp
@@ -1,10 +1,7 @@
-// Добавить в файл CMakeLists.txt своего проекта:
-if (YOUR_DEFINE_VARIABLE)
-    add_definitions(-DYOUR_DEFINE_VARIABLE=1)
-endif (YOUR_DEFINE_VARIABLE)
+// в CMakeLists.txt
+add_definitions(-DYOUR_DEFINE_VARIABLE=1)
 
 // Теперь в main.cpp можно писать
 #ifdef YOUR_DEFINE_VARIABLE
     ...
 #endif
-// И туда не будет заходить в тестирующей системе. Можно объявить свой файловый ввод-вывод
