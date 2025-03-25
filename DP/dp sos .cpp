79278ca24dd73550016e25c4127b3cbe475f5b30
ineyok <redkindany@mail.ru>
2025-03-25 13:30:14 +0400

    Update and rename dp sos  .cpp to dp sos .cpp

--- DP/dp sos .cpp
+++ DP/dp sos .cpp
@@ -1,9 +1,21 @@
+// O(n * log n)
+
 // sum of subsets
 // sos[bit][mask] - сумма по подмаскам mask, которые могут отличаться от mask в первых bit битах
-// O(n * log n)
+
 for(int bit = 0; bit < K; ++bit) {
     for(int mask = 0; mask < (1 << K); ++mask) {
         if((mask >> bit) & 1)
             sos[mask] += sos[mask - (1 << bit)];
+    }
+}
+
+// sum of supersets
+// sos[bit][mask] - ...
+
+for(int bit = 0; bit < K; ++bit) {
+    for(int mask = 0; mask < (1 << K); ++mask) {
+        if((mask >> bit) & 0)
+            sos[mask] += sos[mask + (1 << bit)];
     }
 }
